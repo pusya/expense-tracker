51f35be3b7d56037d7d308f3d06f659c646e090b
Ruoying Jiang <rhoswen827@gmail.com>
2020-03-20 15:40:27 -0400

    navbar & initial home page setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useContext } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: "Expense Tracker",
+  icon: "fas fa-file-signature"
+};
+
+export default Navbar;
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,7 @@
+import React, { useContext, useEffect } from "react";
+
+const Home = () => {
+  return <div className="grid-2">home</div>;
+};
+
+export default Home;
